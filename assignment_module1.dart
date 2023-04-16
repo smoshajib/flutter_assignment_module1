d4fa34dd7e3b30a8b052e6e2a9eb93b1464ec7f9
smo shajib <smoshajib100@gmail.com>
2023-04-17 01:13:49 +0600

    updated

--- assignment_module1.dart
+++ assignment_module1.dart
@@ -1,5 +1,5 @@
 void main() {
-  List<int> phoneNumber = [88, 1768131685, 1768171985, 1768111286, 1768131685];
+  var phoneNumber = [88, 1768131685, 1768171985, 1768111286, 1768131685];
 
   for (int i = 1; i < phoneNumber.length; i++) {
     String formattedNumber = '+880${phoneNumber[i]}';
